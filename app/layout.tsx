00479377eafceb8174564713bc40a7ca76faff76
Koki Kanazono <kokikanazono@koki-kanazono.local>
2023-04-14 20:07:06 +0900

    m1macで表示

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ja">
+      <body>
+        {children}
+      </body>
     </html>
   )
 }
